c23f34e2a086570d365e39a1b91ca5266db3114d
TEOS <proyectoteos@gmail.com>
2018-04-17 00:39:50 -0500

    Code and diagram available
    Program code and assembly scheme

---
+++ Relevador-con-Arduino.ino
@@ -0,0 +1,32 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+  WEB https://goo.gl/jtiYJy
+*/
+
+const int relayPinT = 2;
+String resultSerialT;
+int relayStateT = HIGH;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relayPinT, OUTPUT);
+  digitalWrite(relayPinT, relayStateT);
+}
+
+void loop() {
+  if (relayStateT == LOW)
+    relayStateT = HIGH;
+  else
+    relayStateT = LOW;
+  digitalWrite(relayPinT, relayStateT);
+  resultSerialT = "El estado del relevador es: " + String(relayStateT);
+  Serial.println(resultSerialT);
+  delay(500);
+}
